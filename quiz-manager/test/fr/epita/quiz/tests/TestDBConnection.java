f5c55f79bae00b68c1e455564a5ba86e79a26c3b
Présentation <thomas.broussard@epita.fr>
2022-07-19 15:13:45 +0200

    * JDBC DAO

--- quiz-manager/test/fr/epita/quiz/tests/TestDBConnection.java
+++ quiz-manager/test/fr/epita/quiz/tests/TestDBConnection.java
@@ -5,6 +5,30 @@ import java.sql.*;
 public class TestDBConnection {
 
     public static void main(String[] args) throws SQLException {
+       // testSimpleConnectionAndSelect();
+
+        // when
+        /*?currentSchema=public to be appended to the connection url to specify the schema*/
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/quiz-manager-db", "postgres","postgres");
+
+
+
+        String createTableQuery = "CREATE TABLE STUDENTS( id varchar(30), name varchar(255))";
+
+        connection.prepareStatement(createTableQuery).execute();
+
+        String insertQuery = "INSERT INTO STUDENTS(id, name) values ('thomas@epita.fr', 'thomas')";
+
+        connection.prepareStatement(insertQuery).execute();
+        connection.close();
+
+    }
+
+    private static void testSimpleConnectionAndSelect() throws SQLException {
+        //given
+
+
+        // when
         Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/northwind", "postgres","postgres");
         String schema = connection.getSchema();
         System.out.println(schema);
@@ -18,8 +42,9 @@ public class TestDBConnection {
             System.out.println(resultSet.getString("customer_id"));
         }
 
+        //then
+
+        //cleanup
         connection.close();
-
-
     }
 }
